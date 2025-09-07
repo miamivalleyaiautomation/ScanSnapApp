afec842cdb297b853293b70f642ef4df723d500d
miamivalleyaiautomation <miamivalleyaiautomation@gmail.com>
2025-09-06 23:55:31 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// Enables .vue SFC compilation for Vite builds (both local & Netlify)
 export default defineConfig({
   plugins: [vue()],
-  server: { host: true }
+  server: { host: true }, // allow LAN/mobile testing
+  build: {
+    sourcemap: false,
+    target: 'es2020'
+  },
+  optimizeDeps: {
+    include: ['vue', 'vue-qrcode-reader']
+  }
 })
