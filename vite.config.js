7943b2e1c87f7ec0bcef64777692b402079f1439
miamivalleyaiautomation <miamivalleyaiautomation@gmail.com>
2025-09-06 23:48:44 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// Required so Vite can parse .vue files (SFCs)
 export default defineConfig({
   plugins: [vue()],
-  // optional but handy when testing on device/LAN
   server: { host: true }
 })
