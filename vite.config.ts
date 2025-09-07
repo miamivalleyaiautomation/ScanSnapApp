e3c2fba840920a5a40fd30c194b98d92b9af3f3c
miamivalleyaiautomation <miamivalleyaiautomation@gmail.com>
2025-09-07 00:19:31 -0400

    Create vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+// FILE: vite.config.ts
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// Enables .vue SFC compilation
+export default defineConfig({
+  plugins: [vue()],
+  server: { host: true },
+  build: { target: 'es2020', sourcemap: false },
+  optimizeDeps: { include: ['vue', 'vue-qrcode-reader'] }
+})
